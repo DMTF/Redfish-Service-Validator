cdfb9863ff0be010a0adc42ba138c589cea2d027
Ed Tanous <edtanous@google.com>
2022-07-18 10:48:09 -0700

    Add back top level scripts
    Signed-off-by: Ed Tanous <edtanous@google.com>

---
+++ RedfishServiceValidator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from redfish_service_validator.RedfishServiceValidator import main
+
+if __name__ == '__main__':
+  main()
---
+++ RedfishServiceValidatorGui.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from redfish_service_validator.RedfishServiceValidatorGui import main
+
+if __name__ == '__main__':
+  main()
